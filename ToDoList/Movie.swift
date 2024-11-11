87ce40f86bc4974f9a24407b971a7720ba4406c6
PortableDoughnut <gwen.thelin@gmail.com>
2024-11-11 09:13:19 -0700

    Started setting up the models for the movies and the todo items.

---
+++ ToDoList/Movie.swift
@@ -0,0 +1,28 @@
+//
+//  Movie.swift
+//  ToDoList
+//
+//  Created by Gwen Thelin on 11/11/24.
+//
+
+import Foundation
+import UIKit
+
+struct Movie {
+	var id: UUID
+	var title: String
+	var description: String
+	var poster: UIImage
+}
+
+extension Movie: Equatable {
+	static func == (lhs: Movie, rhs: Movie) -> Bool {
+		lhs.id == rhs.id
+	}
+}
+
+extension Movie: Comparable {
+	static func < (lhs: Movie, rhs: Movie) -> Bool {
+		lhs.title < rhs.title
+	}
+}
---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,38 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Gwen Thelin on 11/11/24.
+//
+
+import Foundation
+import UIKit
+
+struct ToDo {
+	var id: UUID
+	var movie: Movie
+	var hasWatched: Bool
+	var watchedOnDate: Date?
+	var review: String
+	var watchByDate: Date
+}
+
+extension ToDo: Equatable {
+	static func == (lhs: ToDo, rhs: ToDo) -> Bool {
+		return lhs.id == rhs.id
+	}
+}
+
+extension ToDo: Comparable {
+	static func < (lhs: ToDo, rhs: ToDo) -> Bool {
+		if lhs.watchByDate > Date() {
+			if let lhsWatchByDate = lhs.watchedOnDate, let rhsWatchByDate = rhs.watchedOnDate {
+				return lhsWatchByDate < rhsWatchByDate
+			} else {
+				return lhs.movie.title < rhs.movie.title
+			}
+		} else {
+			return lhs.watchByDate < rhs.watchByDate
+		}
+	}
+}
